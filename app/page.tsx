fee9518ba6b68b39f8065f638645bc2a31afc11f
Apple <apple@Apples-MacBook-Pro.local>
2025-06-03 11:31:09 +0100

    file change

--- app/page.tsx
+++ app/page.tsx
@@ -2748,7 +2748,7 @@ const toolsSkills = [
               transition={{ duration: 0.5 }}
               className="text-2xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-pink-500 via-purple-500 to-cyan-500"
             >
-              {"<Deife />"}
+              {"<Deife. />"}
             </motion.div>
 
             {/* Desktop Navigation */}
